e8f48d5942833d599d36f38f68c5af4c0de24cd4
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-29 21:45:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,8 +12,8 @@ attr_accessor :id, :name, :type, :db
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id, db)
-    info = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+  def self.find(id_number, db)
+    info = db.execute("SELECT * FROM pokemon WHERE id = ?", id_number)
     Pokemon.new(id: info[0], name: info[1], type: info[2], db: db)
   end
 
