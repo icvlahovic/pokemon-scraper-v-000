bdb885bbd3e88f4762db65f6993a9fb86fece9b1
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-29 22:03:40 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,8 +18,8 @@ attr_accessor :id, :name, :type, :hp, :db
     Pokemon.new(id: info[0], name: info[1], type: info[2], db: db)
   end
 
-  def alter_hp(hp_number, db)
-    db.execute("UPDATE TABLE pokemon SET hp = ?;", hp_number)
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE TABLE pokemon SET hp = ? WHERE id = ?", new_hp)
   end
 
 end
